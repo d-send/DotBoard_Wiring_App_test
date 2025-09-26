7687d9574e1468e5442f6aa886d6a5b1725ace5a
d-send <Dronecend@gmail.com>
2025-09-26 09:55:57 +0530

    Dot board created on the window.

--- DBW-App/Source/App.cpp
+++ DBW-App/Source/App.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream>1
 #include <string>
 
 #pragma warning(disable : 4996)
@@ -12,17 +12,21 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
+#define Pitch_Between_Pins 2.54  //in mm
+#define Hole_Diameter 1 //in mm
+
 int main() 
 {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
     InitWindow(1280,720, "DBW");
     SetTargetFPS(60);
 
+    /*
     DBW::WIFI::Init();
     DBW::WIFI::Connect("192.168.1.16", 1234);
     DBW::WIFI::SendMsg("Hello world!");
-
-    /*
+    DBW::WIFI::DisConnect();
+   
     std::cout << "Type messages and press ENTER to send. Type 'exit' to quit.\n";
 
     std::string msg;
@@ -36,21 +40,40 @@ int main()
     }
     */
 
+    float factor = 10.0f;
+
+    //top left corner
+    int boardLocation_x = 50;
+    int boardLocation_y = 50;
+
+    int No_Holes_x = 24;
+    int No_Holes_y = 56;
+
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(BLACK);
+        ClearBackground(WHITE);
 
+        //scrolling for zooming in and out
+        factor = factor + GetMouseWheelMove()*0.4;
 
-        double T = GetTime();
-        DrawText( TextFormat("DrawTime : %f s", T),  10,30 , 25, BLUE);
-
-
+        //Dot Board
+        float pitch = Pitch_Between_Pins * factor;
+        DrawRectangle(boardLocation_x- pitch, boardLocation_y- pitch, pitch * (No_Holes_y+1), pitch * (No_Holes_x+1),BROWN);
+        for (int i = 0;i<No_Holes_x;i++)
+        {
+            for (int j = 0;j < No_Holes_y;j++)
+            {
+                DrawCircle(boardLocation_x + pitch * j , boardLocation_y + pitch * i, Hole_Diameter * 0.5f * factor, WHITE);
+            }
+        }
+        //
 
         EndDrawing();
     }
     CloseWindow();
 
-    DBW::WIFI::DisConnect();
+   
     return 0;
 }
